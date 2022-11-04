1efbabbf7a87959ce24d140e8c39b312e957dac7
Juan Camilo Salazar <juancspace@outlook.com>
2022-11-04 09:54:30 -0500

    User's profile details.

---
+++ cride/users/permissions.py
@@ -0,0 +1,11 @@
+"""Users permissions."""
+
+# Django REST Framework
+from rest_framework.permissions import BasePermission
+
+class IsAccountOwner(BasePermission):
+    """Allow access only to objects owned by the requesting user."""
+
+    def has_object_permission(slef, request, view, obj):
+        """Check obj and user are the same."""
+        return request.user == obj
